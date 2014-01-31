21f477af949c47b166f6adfda322d587886d7022
HBSolutions <HBSolutionsgr@hotmail.com>
2014-01-31 21:14:25 +0200

    Test

---
+++ FormMain.Designer.vb
@@ -0,0 +1,193 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FormMain))
+        Me.BtnStart = New System.Windows.Forms.Button()
+        Me.tbCurStatus = New System.Windows.Forms.TextBox()
+        Me.BtnAdAdds = New System.Windows.Forms.Button()
+        Me.nfi = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.CMSnfi = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AddCurrentAdvToListToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditAdsListToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.BtnEditAdsList = New System.Windows.Forms.Button()
+        Me.BtnPrev = New System.Windows.Forms.Button()
+        Me.BtnPlayPause = New System.Windows.Forms.Button()
+        Me.BtnForward = New System.Windows.Forms.Button()
+        Me.CMSnfi.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'BtnStart
+        '
+        Me.BtnStart.Location = New System.Drawing.Point(14, 96)
+        Me.BtnStart.Name = "BtnStart"
+        Me.BtnStart.Size = New System.Drawing.Size(255, 38)
+        Me.BtnStart.TabIndex = 1
+        Me.BtnStart.Text = "Start EZMutify"
+        Me.BtnStart.UseVisualStyleBackColor = True
+        '
+        'tbCurStatus
+        '
+        Me.tbCurStatus.Location = New System.Drawing.Point(13, 29)
+        Me.tbCurStatus.Multiline = True
+        Me.tbCurStatus.Name = "tbCurStatus"
+        Me.tbCurStatus.Size = New System.Drawing.Size(253, 20)
+        Me.tbCurStatus.TabIndex = 2
+        '
+        'BtnAdAdds
+        '
+        Me.BtnAdAdds.Location = New System.Drawing.Point(272, 29)
+        Me.BtnAdAdds.Name = "BtnAdAdds"
+        Me.BtnAdAdds.Size = New System.Drawing.Size(118, 61)
+        Me.BtnAdAdds.TabIndex = 3
+        Me.BtnAdAdds.Text = "Add to Ads"
+        Me.BtnAdAdds.UseVisualStyleBackColor = True
+        '
+        'nfi
+        '
+        Me.nfi.BalloonTipText = "EZMutify is still running"
+        Me.nfi.BalloonTipTitle = "EZMutify"
+        Me.nfi.ContextMenuStrip = Me.CMSnfi
+        Me.nfi.Icon = CType(resources.GetObject("nfi.Icon"), System.Drawing.Icon)
+        Me.nfi.Text = "EZMutify"
+        Me.nfi.Visible = True
+        '
+        'CMSnfi
+        '
+        Me.CMSnfi.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem1, Me.AddCurrentAdvToListToolStripMenuItem, Me.EditAdsListToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.CMSnfi.Name = "CMSnfi"
+        Me.CMSnfi.Size = New System.Drawing.Size(193, 92)
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(192, 22)
+        Me.ToolStripMenuItem1.Text = "Show EZMutify"
+        '
+        'AddCurrentAdvToListToolStripMenuItem
+        '
+        Me.AddCurrentAdvToListToolStripMenuItem.Name = "AddCurrentAdvToListToolStripMenuItem"
+        Me.AddCurrentAdvToListToolStripMenuItem.Size = New System.Drawing.Size(192, 22)
+        Me.AddCurrentAdvToListToolStripMenuItem.Text = "Add Current Ad to List"
+        '
+        'EditAdsListToolStripMenuItem
+        '
+        Me.EditAdsListToolStripMenuItem.Name = "EditAdsListToolStripMenuItem"
+        Me.EditAdsListToolStripMenuItem.Size = New System.Drawing.Size(192, 22)
+        Me.EditAdsListToolStripMenuItem.Text = "Edit Adds List"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(192, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(89, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Spotify is Playing:"
+        '
+        'BtnEditAdsList
+        '
+        Me.BtnEditAdsList.Location = New System.Drawing.Point(273, 96)
+        Me.BtnEditAdsList.Name = "BtnEditAdsList"
+        Me.BtnEditAdsList.Size = New System.Drawing.Size(117, 38)
+        Me.BtnEditAdsList.TabIndex = 5
+        Me.BtnEditAdsList.Text = "Edit Ads List"
+        Me.BtnEditAdsList.UseVisualStyleBackColor = True
+        '
+        'BtnPrev
+        '
+        Me.BtnPrev.BackgroundImage = Global.EZMutify.My.Resources.Resources.PlayPrev
+        Me.BtnPrev.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.BtnPrev.Location = New System.Drawing.Point(13, 62)
+        Me.BtnPrev.Name = "BtnPrev"
+        Me.BtnPrev.Size = New System.Drawing.Size(81, 28)
+        Me.BtnPrev.TabIndex = 6
+        Me.BtnPrev.UseVisualStyleBackColor = True
+        '
+        'BtnPlayPause
+        '
+        Me.BtnPlayPause.BackgroundImage = Global.EZMutify.My.Resources.Resources.Play
+        Me.BtnPlayPause.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.BtnPlayPause.Location = New System.Drawing.Point(100, 62)
+        Me.BtnPlayPause.Name = "BtnPlayPause"
+        Me.BtnPlayPause.Size = New System.Drawing.Size(81, 28)
+        Me.BtnPlayPause.TabIndex = 7
+        Me.BtnPlayPause.UseVisualStyleBackColor = True
+        '
+        'BtnForward
+        '
+        Me.BtnForward.BackgroundImage = Global.EZMutify.My.Resources.Resources.PlayNext
+        Me.BtnForward.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.BtnForward.Location = New System.Drawing.Point(186, 62)
+        Me.BtnForward.Name = "BtnForward"
+        Me.BtnForward.Size = New System.Drawing.Size(81, 28)
+        Me.BtnForward.TabIndex = 8
+        Me.BtnForward.UseVisualStyleBackColor = True
+        '
+        'FormMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(400, 146)
+        Me.Controls.Add(Me.BtnForward)
+        Me.Controls.Add(Me.BtnPlayPause)
+        Me.Controls.Add(Me.BtnPrev)
+        Me.Controls.Add(Me.BtnEditAdsList)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.BtnAdAdds)
+        Me.Controls.Add(Me.tbCurStatus)
+        Me.Controls.Add(Me.BtnStart)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "FormMain"
+        Me.Text = "EZMutify"
+        Me.CMSnfi.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents BtnStart As System.Windows.Forms.Button
+    Friend WithEvents tbCurStatus As System.Windows.Forms.TextBox
+    Friend WithEvents BtnAdAdds As System.Windows.Forms.Button
+    Friend WithEvents nfi As System.Windows.Forms.NotifyIcon
+    Friend WithEvents CMSnfi As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents ToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AddCurrentAdvToListToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents EditAdsListToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents BtnEditAdsList As System.Windows.Forms.Button
+    Friend WithEvents BtnPrev As System.Windows.Forms.Button
+    Friend WithEvents BtnPlayPause As System.Windows.Forms.Button
+    Friend WithEvents BtnForward As System.Windows.Forms.Button
+
+End Class
---
+++ FormMain.vb
@@ -0,0 +1,286 @@
+﻿Imports System.Diagnostics
+Imports CoreAudioApi
+Imports System.IO
+Imports System.Text
+Imports System.Threading
+
+Public Class FormMain
+
+    Dim devEnum As MMDeviceEnumerator = New MMDeviceEnumerator()
+    Dim SndDevice As MMDevice
+    Dim audioSesCntrl As AudioSessionControl
+    Dim spotifyAudioCntrl As AudioSessionControl
+    Dim SndDevCnt As Integer
+    Dim SpotifyRunning As Boolean
+    Dim Processes() As Process
+    Dim spotifyProc As Process
+    Dim spotifyAdds As New List(Of String)
+    Dim spotifyMainTitle As String
+    Dim path As String = Replace(System.IO.Path.GetDirectoryName( _
+        System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase), "file:\", "")
+    Dim adsListPath As String = path & "\Advertisments List.txt"
+    Dim sSpotifyPlayingAd = "EZMutify is silencing an ad"
+    Dim sSpotifyNotRunning As String = "Spotify is not running - Plz start Spotify"
+    Dim sSpotifyNotPlayingSong As String = "Spotify is not playing any song"
+    Dim sSpotifyAd As String = "Spotify - Spotify – Spotify"
+    Dim ReservedStrings() As String = {sSpotifyNotRunning, sSpotifyNotPlayingSong, sSpotifyAd, sSpotifyPlayingAd}
+    Dim spotifyControl As New Spotify
+    Dim storedSpotifyVol As Single
+    Dim muteMasterVol As Single = 5 / 100 '(Range from 0 to 1.0)
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles BtnStart.Click
+
+        If BtnStart.Text = "Stop EZMutify" Then
+            BtnStart.Text = "Start EZMutify"
+            Exit Sub
+        Else
+            BtnStart.Text = "Stop EZMutify"
+        End If
+
+        If Not System.IO.File.Exists(adsListPath) Then
+            System.IO.File.Create(adsListPath).Dispose()
+        End If
+
+        Application.DoEvents()
+        Dim adPlaying As Boolean = False
+        Me.Focus()
+
+        storedSpotifyVol = audioSesCntrl.SimpleAudioVolume.MasterVolume
+
+        Do While SpotifyRunning = True
+            Application.DoEvents()
+            'Reading the adds here mutes the new added ads
+            Dim ad As String = ""
+            Using sr As New StreamReader(adsListPath)
+                Do Until sr.EndOfStream
+                    ad = sr.ReadLine.ToString
+                    spotifyAdds.Add(ad)
+                Loop
+            End Using
+
+            Try
+                spotifyProc.Refresh()
+                spotifyMainTitle = spotifyProc.MainWindowTitle.ToString
+            Catch ex As Exception
+                SpotifyRunning = False
+            End Try
+
+            Processes = Process.GetProcessesByName("Spotify")
+            If Processes.Count = 0 Then
+                SpotifyRunning = False
+                tbCurStatus.Text = sSpotifyNotRunning
+            End If
+
+            If spotifyMainTitle <> "Spotify" Then
+                Try
+                    tbCurStatus.Text = spotifyMainTitle.ToString.Remove(0, 10)
+                    adPlaying = False
+                Catch ex As Exception
+                    Debug.Print(ex.ToString)
+                End Try
+            Else
+                tbCurStatus.Text = sSpotifyNotPlayingSong
+            End If
+
+            If spotifyMainTitle = sSpotifyAd Then
+                adPlaying = True
+                'After Update Spotify does not play the ad if in muted state
+                'audioSesCntrl.SimpleAudioVolume.Mute = True
+                If spotifyAudioCntrl.SimpleAudioVolume.MasterVolume <> muteMasterVol Then
+                    storedSpotifyVol = audioSesCntrl.SimpleAudioVolume.MasterVolume
+                End If
+                spotifyAudioCntrl.SimpleAudioVolume.MasterVolume = muteMasterVol
+            Else
+                For Each ad In spotifyAdds
+                    If spotifyMainTitle = "Spotify - " & ad Then
+                        adPlaying = True
+                        'audioSesCntrl.SimpleAudioVolume.Mute = True
+                        If spotifyAudioCntrl.SimpleAudioVolume.MasterVolume <> muteMasterVol Then
+                            storedSpotifyVol = audioSesCntrl.SimpleAudioVolume.MasterVolume
+                        End If
+                        spotifyAudioCntrl.SimpleAudioVolume.MasterVolume = muteMasterVol
+                        tbCurStatus.Text = sSpotifyPlayingAd
+                    End If
+                Next
+            End If
+
+            If adPlaying = False Then
+                Thread.Sleep(300)
+                spotifyAudioCntrl.SimpleAudioVolume.MasterVolume = storedSpotifyVol
+                'audioSesCntrl.SimpleAudioVolume.Mute = False
+            End If
+        Loop
+
+    End Sub
+
+    Private Function PlayState() As Boolean
+        spotifyProc.Refresh()
+        spotifyMainTitle = spotifyProc.MainWindowTitle.ToString
+        If spotifyMainTitle = "Spotify" Then
+            BtnPlayPause.BackgroundImage = My.Resources.Play
+            Return False
+        Else
+            BtnPlayPause.BackgroundImage = My.Resources.Pause
+            Return True
+        End If
+    End Function
+
+    Private Sub BtnAdAdds_Click(sender As Object, e As EventArgs) Handles BtnAdAdds.Click
+
+        Me.Focus()
+        If Not System.IO.File.Exists(adsListPath) Then
+            System.IO.File.Create(adsListPath).Dispose()
+        End If
+
+        Dim sb As New StringBuilder()
+        Dim line As String
+
+        Using sr As New StreamReader(adsListPath)
+
+            For Each add In ReservedStrings
+                If tbCurStatus.Text = add Then
+                    Exit Sub
+                End If
+            Next
+
+            Do Until sr.EndOfStream
+                line = sr.ReadLine()
+                If line = tbCurStatus.Text Then
+                    MsgBox("Advertisment already in the list")
+                    Exit Sub
+                End If
+            Loop
+            sr.BaseStream.Position = 0
+            sb.Append(sr.ReadToEnd())
+            sb.AppendLine(tbCurStatus.Text)
+        End Using
+
+        Using outfile As New StreamWriter(adsListPath)
+            outfile.Write(sb.ToString())
+        End Using
+        MsgBox("Advertisment added to the Ads list")
+
+    End Sub
+
+    Private Sub FormMain_FormClosing(ByVal sender As Object, ByVal e As FormClosingEventArgs) Handles Me.FormClosing
+        'First minimize the form
+        Me.WindowState = FormWindowState.Minimized
+        'Now make it invisible (make it look like it went into the system tray)
+        Me.Visible = False
+        'Cancel the closing of the application
+        e.Cancel = True
+        nfi.Visible = True
+        nfi.ShowBalloonTip(2000)
+        'MsgBox("FTProgram has been minimized to the task bar.")
+
+    End Sub
+
+    Private Sub nfi_MouseDown(sender As Object, e As MouseEventArgs) Handles nfi.MouseDown
+        If Me.Visible = False Then
+            If e.Button = MouseButtons.Left Then
+                Me.Show()
+                Me.BringToFront()
+                Me.WindowState = FormWindowState.Normal
+            End If
+        Else
+            If e.Button = MouseButtons.Left Then Me.Hide()
+        End If
+    End Sub
+
+    Private Sub ToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem1.Click
+        Me.Show()
+        Me.BringToFront()
+        Me.WindowState = FormWindowState.Normal
+    End Sub
+
+    Private Sub AddCurrentAdvToListToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AddCurrentAdvToListToolStripMenuItem.Click
+        Call BtnAdAdds_Click(sender, e)
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        'Unmute in case user exits program in middle of ad
+        Try
+            If audioSesCntrl.SimpleAudioVolume.Mute = True Then
+                audioSesCntrl.SimpleAudioVolume.Mute = False
+            End If
+        Catch ex As Exception
+        Finally
+            End
+        End Try
+
+        Try
+            If spotifyAudioCntrl.SimpleAudioVolume.MasterVolume = 5 Then
+                spotifyAudioCntrl.SimpleAudioVolume.MasterVolume = storedSpotifyVol
+            End If
+        Catch ex As Exception
+        Finally
+            End
+        End Try
+
+        End
+    End Sub
+
+    Private Sub EditAdsListToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles EditAdsListToolStripMenuItem.Click
+        If Not System.IO.File.Exists(adsListPath) Then
+            System.IO.File.Create(adsListPath).Dispose()
+        Else
+            System.Diagnostics.Process.Start(adsListPath)
+        End If
+    End Sub
+
+    Private Sub BtnEditAdsList_Click(sender As Object, e As EventArgs) Handles BtnEditAdsList.Click
+        If Not System.IO.File.Exists(adsListPath) Then
+            System.IO.File.Create(adsListPath).Dispose()
+        Else
+            System.Diagnostics.Process.Start(adsListPath)
+        End If
+    End Sub
+
+    Private Sub BtnPrev_Click(sender As Object, e As EventArgs) Handles BtnPrev.Click
+        spotifyControl.PlayPrev()
+    End Sub
+
+    Private Sub BtnForward_Click(sender As Object, e As EventArgs) Handles BtnForward.Click
+        spotifyControl.PlayNext()
+    End Sub
+
+    Private Sub BtnPlayPause_Click(sender As Object, e As EventArgs) Handles BtnPlayPause.Click
+        spotifyControl.PlayPause()
+        PlayState()
+    End Sub
+
+    Private Sub FormMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        BtnStart.Text = "Start EZMutify"
+        'Check if Spotify is active
+        Processes = Process.GetProcessesByName("Spotify")
+        If Processes.Count = 0 Then
+            SpotifyRunning = False
+            tbCurStatus.Text = sSpotifyNotRunning
+            Exit Sub
+        Else
+            spotifyProc = Processes(0)
+            SpotifyRunning = True
+            spotifyMainTitle = spotifyProc.MainWindowTitle.ToString
+            SndDevice = devEnum.GetDefaultAudioEndpoint(EDataFlow.eRender, ERole.eMultimedia)
+            SndDevCnt = SndDevice.AudioSessionManager.Sessions.Count
+
+            For i = 0 To SndDevCnt - 1
+                audioSesCntrl = SndDevice.AudioSessionManager.Sessions.Item(i)
+                If audioSesCntrl.SessionIdentifier.Contains("spotify.exe") Then
+                    spotifyAudioCntrl = SndDevice.AudioSessionManager.Sessions.Item(i)
+                    spotifyAudioCntrl.SimpleAudioVolume.Mute = False
+                    Exit For
+                End If
+            Next
+
+        End If
+
+        PlayState()
+    End Sub
+
+    Private Sub tbCurStatus_TextChanged(sender As Object, e As EventArgs) Handles tbCurStatus.TextChanged
+        PlayState()
+    End Sub
+
+End Class
